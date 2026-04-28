db37544dbe2aefc9c5f603913ae344d71df3b3f0
Michael Meyerson <mmeyerson@seatgeek.com>
2026-04-27 20:27:35 -0700

    Exercise 3

--- src/components/Guesses/Guesses.js
+++ src/components/Guesses/Guesses.js
@@ -1,11 +1,15 @@
 import React from "react";
+import { range } from "../../utils";
+import { NUM_OF_GUESSES_ALLOWED, WORD_LENGTH } from "../../constants";
 
 function Guesses({ guesses = [] }) {
   return (
     <div className="guess-results">
-      {guesses.map((guess) => (
-        <p key={guess} className="guess">
-          {guess}
+      {range(0, NUM_OF_GUESSES_ALLOWED).map((rowCount) => (
+        <p key={rowCount} className="guess">
+          {range(0, WORD_LENGTH).map((colCount) => (
+            <span key={colCount} className="cell">{guesses[rowCount]?.[colCount] ?? ''}</span>
+          ))}
         </p>
       ))}
     </div>
