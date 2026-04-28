2964b45439bda324bc4fcba7bd857b56646b72ce
Michael Meyerson <mmeyerson@seatgeek.com>
2026-04-27 20:23:34 -0700

    Exercise 2

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -3,6 +3,7 @@ import React from "react";
 import { sample } from "../../utils";
 import { WORDS } from "../../data";
 import Input from "../Input";
+import Guesses from "../Guesses";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -10,13 +11,16 @@ const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
+  const [guesses, setGuesses] = React.useState([]);
 
   const handleSubmit = (guess) => {
     console.log(guess);
+    setGuesses([...guesses, guess]);
   };
 
   return (
     <>
+      <Guesses guesses={guesses} />
       <Input onSubmit={handleSubmit} />
     </>
   );
